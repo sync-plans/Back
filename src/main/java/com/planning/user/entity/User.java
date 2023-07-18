7904e57e8398e1e64ad62451812f6afb16c38ed5
yunji <dbswl4342@gmail.com>
2023-07-18 13:27:16 +0900

    fix : username unique 추가

--- src/main/java/com/planning/user/entity/User.java
+++ src/main/java/com/planning/user/entity/User.java
@@ -18,7 +18,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String username;
 
     @Column(nullable = false)
