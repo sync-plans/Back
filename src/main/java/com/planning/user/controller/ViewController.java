22f4922e2c1dfda47a3d0135f3cdd4820c0ea07e
cornpip <chdnjf1375@naver.com>
2023-07-19 11:40:39 +0900

    fix : kakao 병합 부분수정(2)

---
+++ src/main/java/com/planning/user/controller/ViewController.java
@@ -0,0 +1,19 @@
+package com.planning.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class ViewController {
+
+    @GetMapping("/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+}
